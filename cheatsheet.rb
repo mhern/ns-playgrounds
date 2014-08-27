7ad0200b9fbd326bfd98ec5a50c1640a07d8b4c7
Melissa Hernandez <mhernandez@netsocket.com>
2014-08-27 18:14:31 -0500

    updating my cheat sheet with new things I learned today

--- cheatsheet.rb
+++ cheatsheet.rb
@@ -32,6 +32,13 @@ one = 1 / 1		#divison
 one = 1 ** 1	#exponentiation
 one = 1 % 2		#modulo
 
+#NOTE ++ and -- are not supported
+#but +=, -=, *=, and /= are
+one += 1
+one -= 1
+one *= 1
+one /= 1
+
 #comparision
 #if, elsif, else, unless, end
 if 1 < 2
@@ -75,12 +82,17 @@ no_new_line = gets.chomp
 puts 'puts adds a newline after the line printed'
 print 'print does not'
 
+#concatination is with a plus
+puts 'hello' + ' ' + 'world'
+
 #string methods we went over
 str = 'Hello World'		#use ' when nothing needs to be evaluated (take string literally)
 puts "#{str.reverse}" 	#use " when something needs to be evaluated
 puts "#{str.capitalize}"
 str_all_caps = "#{str.upcase}"
 str_all_lowercase = str_all_caps.downcase
+str_all_caps.gsub!(/ABC/, " ") 				#gsub = global substitution
+array_of_strings = str.split(" ")			#split supported
 str = str_all_lowercase.reverse.upcase 		#methods can be chained
 
 
@@ -96,8 +108,78 @@ puts "value of this_is_true: #{this_is_true.to_s}"
 
 # ! modifies in place
 with_new_line.capitalize!
-no_new_line.reverse!
+#and goes before the arguments to the method (no space between!)
+no_new_line.gsub!(/s/, "th")
 
+#methods ending in ? evaluate to a boolean value as a general rule
+a_boolean = no_new_line.include? " "
+a_boolean = true
+
+
+=begin
+=
+= 	LOOPS
+=
+=end
+
+#while loop
+while false
+	puts "this should never run"
+end
+
+#until loop
+until true
+	puts "this should never run"
+end
+
+#for in loop
+#... is exclusive, this prints 1 - 9
+for num in 1...10
+	puts num
+end
+#.. is inclusive, this prints 1 - 10
+for num in 1..10
+	puts num
+end
+
+#loop break loop
+i = 0
+loop {
+	i+= 1
+	break if i > 5
+}
+#equivalent to
+i = 0
+loop do
+	i += 1
+	break if i > 5
+end
+
+#each loop (on an array)
+my_array = [1,2,3,4,5]
+my_array.each do |x|
+	puts "#{x}"
+end
+
+#times loop (on a number)
+5.times { print "iteration" }
+
+#loop keywords
+#next - skip steps in a loop
+for num in 1..5
+	next unless i % 2 == 0
+	print num
+end
+
+
+=begin
+=
+= 	BLOCKS
+=
+=end
+
+#for a code block: { <something here> } is eqivalent to do <something here> end
+# so ({ ~= do) and (} ~= end)
 
 =begin
 =
