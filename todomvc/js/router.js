aff1b02b76406816e00a9e0dc42608d39b0c6d86
Melissa Hernandez <mhernandez@netsocket.com>
2014-11-04 11:55:39 -0600

    use handlebars bind-attr to make the class on an li "completed" if isCompleted

--- todomvc/js/router.js
+++ todomvc/js/router.js
@@ -2,7 +2,7 @@
 
 //router for Todos
 Todos.Router.map(function(){
-    this.resource('todos', {'path': '/'}); //detect when the URL matches '/' and render the 'todo-list' template
+    this.resource('todos', {'path': '/'}); //detect when the URL matches '/' and render the 'todos' template
 });
 
 //add class for custom behavior: return all existing todos
