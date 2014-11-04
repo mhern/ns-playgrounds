3d948a0c21ea924d68d1b478544a3b62d1da8262
Melissa Hernandez <mhernandez@netsocket.com>
2014-11-04 11:51:46 -0600

    the instructions don't explain, but you have to move the html into handlebars templates for the {{#each}} to work

--- todomvc/js/router.js
+++ todomvc/js/router.js
@@ -2,5 +2,12 @@
 
 //router for Todos
 Todos.Router.map(function(){
-    this.resource('todos', {'path': '/'}); //detect when the URL matches '/' and render the 'todos' template
+    this.resource('todos', {'path': '/'}); //detect when the URL matches '/' and render the 'todo-list' template
+});
+
+//add class for custom behavior: return all existing todos
+Todos.TodosRoute = Ember.Route.extend({
+    model : function(){
+        return this.store.find('todo');
+    }
 });
