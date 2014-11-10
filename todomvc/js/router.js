e24ced51b9a2f2e24df9e26efa992a5ead21565b
Melissa Hernandez <mhernandez@netsocket.com>
2014-11-10 10:54:57 -0600

    move todo-list into its own template

--- todomvc/js/router.js
+++ todomvc/js/router.js
@@ -2,12 +2,24 @@
 
 //router for Todos
 Todos.Router.map(function(){
-    this.resource('todos', {'path': '/'}); //detect when the URL matches '/' and render the 'todos' template
+    this.resource('todos',
+        {'path': '/'}, //detect when the URL matches '/' and render the 'todos' template
+        function(){
+            //additional child routes will go here later
+        }
+    );
 });
 
 //add class for custom behavior: return all existing todos
 Todos.TodosRoute = Ember.Route.extend({
     model : function(){
         return this.store.find('todo');
+    }
+});
+
+//corresponds with todos.index route and todos/index template
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model : function(){
+        return this.modelFor('todos'); //model for this route is the same model as for the TodosRoute
     }
 });
