dbf7d0349eb2c3f0b3d1ceba8834fa1c761a8201
Melissa Hernandez <mhernandez@netsocket.com>
2014-11-18 06:48:53 -0600

    move away from fixture data, to browser local storage

--- todomvc/js/application.js
+++ todomvc/js/application.js
@@ -4,5 +4,11 @@
 window.Todos = Ember.Application.create();
 
 //ApplicationAdapter communicates with the source of data for app;
+
 //FixtureAdapter is an adapter designed to load fixture data
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+//Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+//use local storage capability
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace : 'todos-emberjs'
+});
--- todomvc/js/models/todo.js
+++ todomvc/js/models/todo.js
@@ -6,21 +6,23 @@ Todos.Todo = DS.Model.extend({
     isCompleted: DS.attr('boolean')
 });
 
-//add fixture data for now
-Todos.Todo.FIXTURES = [
-    {
-        id: 1,
-        title: 'Learn Ember.js',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        title: '...',
-        isCompleted: false
-    },
-    {
-        id: 3,
-        title: 'Profit!',
-        isCompleted: false
-    }
-];
+
+//this is no longer used since we changed from using the FixtureAdapter to LSAdapter (local-storage)
+//example of fixture data that would work with the application's fixture adapter:
+// Todos.Todo.FIXTURES = [
+//     {
+//         id: 1,
+//         title: 'Learn Ember.js',
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         title: '...',
+//         isCompleted: false
+//     },
+//     {
+//         id: 3,
+//         title: 'Profit!',
+//         isCompleted: false
+//     }
+// ];
